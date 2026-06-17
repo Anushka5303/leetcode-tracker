e9396829b0ed271f63c3913db4e13e0e7f4137de
Anushka Gupta <128627834+Anushka5303@users.noreply.github.com>
2026-06-18 03:37:13 +0530

    Time: 235 ms (5.02%) | Memory: 22.9 MB (6.03%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-
-        if(nums.size() <= 1)
-            return 0;
+        if(nums.size()<=1) return 0;
         vector<int> dp(nums.size(),-1);
-        return magic(nums, 0, dp);
+
+        dp[nums.size()-1] = 0;
+        return magic(nums,0,dp);
     }
-
-
 
 private:
-    int magic(vector<int>& nums, int i, vector<int>& dp) {
+    int magic(vector<int>& nums,int i, vector<int>& dp){
+        if(i>nums.size()-1) return dp[i];
 
-        if(i >= nums.size() - 1)
-            return 0;
+        if(nums[i]==0 && i!=nums.size()-1) return INT_MAX;
 
-        if(dp[i] != -1)
-            return dp[i];
+        if(dp[i] != -1) return dp[i];
 
+        int jumps = nums[i];
         int mini = INT_MAX;
 
-        for(int j = 1; j <= nums[i]; j++) {
-
-            int child = magic(nums, i + j, dp);
-
-            if(child != INT_MAX)
-                mini = min(mini, 1 + child);
+        for(int j=1; j<=jumps && i+j<nums.size(); j++){
+            int res = magic(nums,i+j,dp);
+            
+            if(res !=INT_MAX)
+                mini = min(mini,1+res);
         }
-
         return dp[i] = mini;
     }
-
-
 };
