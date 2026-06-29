056739f4ad7d76e942f31c0df535742ce6be8353
Anushka Gupta <128627834+Anushka5303@users.noreply.github.com>
2026-06-30 01:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.61%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,34 +3,51 @@ public:
     int rob(vector<int>& nums) {
         int ans = INT_MIN;
         int n = nums.size();
+        if(n==1) return nums[0];
         vector<int> dp(n,-1);
         dp[n-1] = nums[n-1];
-        return magic(nums,0,ans,dp);
+        // dp[n-2] = nums[n-2];
+
+        
+        
+        // return magic(nums,0,ans,dp);
         // return ans;
+        for(int i=n-2; i>=0; i--){
+            int take = nums[i];
+            if(i+2<n) take += dp[i+2];
+            
+            int notTake = 0 + dp[i+1];
+
+            int ans = max(take,notTake);
+
+            dp[i] = ans;
+        }
+        return dp[0];
+       
     }
-private:
-int magic(vector<int>& nums,int i,int& ans,vector<int>& dp){
-    if(i==nums.size()-1) return dp[i];
+// private:
+// int magic(vector<int>& nums,int i,int& ans,vector<int>& dp){
+//     if(i==nums.size()-1) return dp[i];
 
-    if(dp[i] != -1) return dp[i];
+//     if(dp[i] != -1) return dp[i];
 
-    int take = INT_MIN, notTake = INT_MIN;
+//     int take = INT_MIN, notTake = INT_MIN;
 
-    if(i+2<nums.size()) take = magic(nums,i+2,ans,dp);
+//     if(i+2<nums.size()) take = magic(nums,i+2,ans,dp);
 
-    if(take != INT_MIN){
-        take += nums[i];
-    }else{
-        take = nums[i];
-    }
+//     if(take != INT_MIN){
+//         take += nums[i];
+//     }else{
+//         take = nums[i];
+//     }
 
 
-    if(i+1<nums.size()) notTake = magic(nums,i+1,ans,dp);
+//     if(i+1<nums.size()) notTake = magic(nums,i+1,ans,dp);
 
-    int maxi = max(take,notTake);
+//     int maxi = max(take,notTake);
 
-    ans=max(ans,maxi);
+//     ans=max(ans,maxi);
 
-    return dp[i] = maxi;
-}
+//     return dp[i] = maxi;
+// }
 };
