7e0d035fdb5d7892c181da65f106c7765dea70f7
Anushka Gupta <128627834+Anushka5303@users.noreply.github.com>
2026-07-07 00:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (29.45%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n%2==0;
+    }
+};
+
+//bhagwan ji am getting dusturb seriously plz help.... i am genuinly studying plzzzzz helpppp !!!!! plzzz
