b57c3c314902a866fda0c30147c08a7c9c565912
Anushka Gupta <128627834+Anushka5303@users.noreply.github.com>
2026-07-29 01:41:16 +0530

    Time: 153 ms (15.84%) | Memory: 68.8 MB (70.93%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        map<char,int> mp;
+        int n = s.size();
+
+        for(int i=0; i<n; i++){
+            mp[s[i]]++;
+        }
+
+        vector<char> ans(n); 
+
+        int i=0;
+
+        int n2 = mp.size();
+
+        for(auto m: mp){
+            int c = m.second/2;
+            int rem = m.second%2;
+
+            while(c){
+                ans[i]=m.first;
+                ans[n-1-i]=m.first;
+                c--;
+                i++;
+            }
+
+            if(rem==0){
+                m.second=0;
+            }else{
+                m.second = 1;
+            }
+
+        }
+
+        for (auto m : mp) {
+            if (m.second % 2 == 1) {
+                ans[n / 2] = m.first;
+                break;
+            }
+        }
+
+        return string(ans.begin(), ans.end());
+    }
+};
